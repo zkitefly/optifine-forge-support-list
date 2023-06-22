feee08be30240d528c493df093def719d2c63d15
2573874409@qq.com <z18344203426@qq.com>
2023-06-23 01:04:44 +0800

    add

--- o.py
+++ o.py
@@ -10,6 +10,7 @@ if os.path.exists("versionlist"):
 # 下载 versionList.json 文件
 url = "https://bmclapi2.bangbang93.com/optifine/versionList"
 response = requests.get(url)
+response.raise_for_status()  # 检查请求是否成功
 version_list = response.json()
 
 # 保存 versionList.json 文件
@@ -17,7 +18,7 @@ with open("versionlist.json", "w") as file:
     json.dump(version_list, file)
 
 # 创建文件夹 versionlist
-os.mkdir("versionlist")
+os.makedirs("versionlist", exist_ok=True)  # 使用 os.makedirs 递归创建文件夹
 
 # 遍历 versionList.json 中的每个元素
 for version_entry in version_list:
@@ -30,6 +31,7 @@ for version_entry in version_list:
         mcversion_value = version_entry.get("mcversion")
         forge_url = f"https://bmclapi2.bangbang93.com/forge/minecraft/{mcversion_value}"
         forge_response = requests.get(forge_url)
+        forge_response.raise_for_status()  # 检查请求是否成功
         forge_list = forge_response.json()
         for forge_entry in forge_list:
             forge_build = str(forge_entry.get("build"))
@@ -56,14 +58,14 @@ for version_entry in version_list:
             file.write(f"# Minecraft `{mcversion_value}` OptiFine `{type_value}_{patch_value}` 不支持**任何** Forge 版本\n\n")
             file.write("## 你可以前往 [→此处←](./all.md) 查看全部支持情况，找一个相邻的版本进行调整。\n\n")
             file.write("### HMCL\n\n")
-            file.write("#### 你需要在 HMCL 的 `版本设置` -> `自动安装` 中调整 Optifine 版本或（/和） Forge 版本，或将 Optifine 或 Forge 卸载。\n\n![hmcl](/hmcl.gif)\n\n")
+            file.write("#### 你需要在 HMCL 的 `版本设置` -> `自动安装` 中调整 Optifine 版本或（/和） Forge 版本，或将 Optifine 或 Forge 卸载。\n\n![hmcl](hmcl.gif)\n\n")
             file.write("### 其他启动器\n\n")
             file.write("#### 重新安装 Optifine 或 Forge 选其中一个。\n\n")
             file.write(f"##### [→点此处下载该版本 Optifine←](https://optifine.cn/download/{filename})\n\n")
         else:
             file.write(f"# Minecraft `{mcversion_value}` OptiFine `{type_value}_{patch_value}` 支持 `{helpforge}` 版本\n\n")
             file.write("### HMCL\n\n")
-            file.write("#### 你需要在 HMCL 的 `版本设置` -> `自动安装` 中调整 Optifine 版本或（/和） Forge 版本。\n\n![hmcl](/hmcl.gif)\n\n")
+            file.write("#### 你需要在 HMCL 的 `版本设置` -> `自动安装` 中调整 Optifine 版本或（/和） Forge 版本。\n\n![hmcl](hmcl.gif)\n\n")
             file.write("### 其他启动器\n\n")
             file.write("#### 重新安装 Optifine 和 Forge，安装时请留意版本！\n\n")
             file.write(f"##### [→点此处下载该版本 Optifine←](https://optifine.cn/download/{filename}) | [→点击此处下载该版本所支持的 Forge 安装器←](https://maven.minecraftforge.net/net/minecraftforge/forge/{mcversion_value}-{forged}/forge-{mcversion_value}-{forged}-installer.jar)\n\n")
